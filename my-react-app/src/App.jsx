978875728c1e61128d0a9e3f5de8b354254d8dd4
watercuba <watercubz-dev>
2023-05-06 13:10:13 -0400

    Login with talwind css and reactjs

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import LoginForm from './Login/LoginForm'
+
+function App() {
+
+  return (
+    <>
+     <LoginForm />
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Login/LoginForm.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function LoginForm() {
+  return (
+    <div className="flex w-full h-screen">
+        <div className="w-full flex items-center justify-center">
+           <h1>Login</h1>
+       </div>
+      <div className="hidden lg:flex h-full bg-gray-200">
+         <div>hello</div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
